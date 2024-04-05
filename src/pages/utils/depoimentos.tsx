97c50b099460a95cbd8b8158123075dfdb4ea321
Guilherme <gui.bellissimo@gmail.com>
2024-04-05 11:48:27 -0300

    insert new frames

--- src/pages/utils/depoimentos.tsx
+++ src/pages/utils/depoimentos.tsx
@@ -8,39 +8,45 @@ export const depoimentos: any = {
       iframe: (
         <iframe
           width="100%"
-          src="https://www.youtube.com/embed/EcIjBCgyyVk"
-          title="Ceará do Gás fala sobre como é morar no Recanto Paraiso em Rio Claro - SP"
+          src="https://www.youtube.com/embed/7lzR6INCFas"
+          title="Vanderlau fala como é morar no bairro Recanto Paraiso em Rio Claro - SP"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
       ),
     },
     {
       text: "É um bairro tranquilo. Tem as paisagens e as pessoas que são bastante amigas. É bem respeitoso e bem agradável. É bom de morar aqui.",
-      name: "Thais Helena de Camargo Leite",
-      sub: "Balconista",
+      name: "Bruno Alexandre Rossi",
+      sub: "Farmacêutico",
       img: "",
       iframe: (
         <iframe
           width="100%"
-          src="https://www.youtube.com/embed/kfIll4lyCF0"
-          title="Thais fala como é morar no bairro Recanto Paraiso em Rio Claro - SP"
+          src="https://www.youtube.com/embed/4vAbn-HETls"
+          title="Bruno fala como é morar no bairro Recanto Paraiso em Rio Claro - SP"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
       ),
     },
     {
       text: "É um bairro supertranquilo. Já viajei e já deixei minha casa sozinha e graça a Deus ninguém mexe. E agora está crescendo bastante, eu tô feliz",
-      name: "Janete Aparecida da Silva",
-      sub: "Comerciante",
+      name: "Agnaldo Zumpano",
+      sub: "Serralheiro",
       img: "",
       iframe: (
         <iframe
           width="100%"
-          src="https://www.youtube.com/embed/VUu_ZrUpQJA"
-          title="Janete fala sobre como é morar no bairro Recanto Paraiso em Rio Claro - SP"
+          src="https://www.youtube.com/embed/cUq2R34sYpg"
+          title="Agnaldo fala como é morar no bairro Recanto Paraiso em Rio Claro - SP"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
       ),
